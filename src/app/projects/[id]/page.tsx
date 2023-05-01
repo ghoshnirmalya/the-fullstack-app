30e919459eb6e09de1e456b302fed2c08b796441
Nirmalya Ghosh <nirmalya.email@gmail.com>
2023-05-01 22:44:25 +0530

    feat: Adds dummy forums and blogs UI

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,6 +1,15 @@
 import { ProjectEditForm } from "@/components/ProjectEditForm";
 import { getApiUrl } from "@/lib/get-api-url";
 import { Project } from "@prisma/client";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 interface IndexPageProps {
   params: {
@@ -22,9 +31,45 @@ export default async function ProjectShowPage({
   }
 
   return (
-    <div className="p-4 space-y-4 container mx-auto">
-      <div className="w-full md:w-1/2">
+    <div className="p-4 gap-4 container mx-auto flex flex-col lg:flex-row">
+      <div className="w-full lg:w-1/2">
         <ProjectEditForm project={project} />
+      </div>
+      <div className="w-full lg:w-1/2">
+        <Tabs defaultValue="forums">
+          <TabsList>
+            <TabsTrigger value="forums">Forums</TabsTrigger>
+            <TabsTrigger value="blogs">Blogs</TabsTrigger>
+          </TabsList>
+          <TabsContent value="forums">
+            <Card>
+              <CardHeader>
+                <CardTitle>Forum Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="blogs">
+            <Card>
+              <CardHeader>
+                <CardTitle>Blog Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
   );
