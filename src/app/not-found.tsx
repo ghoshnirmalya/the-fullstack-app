ac15652da4f226dbbb450c5f9004ac780d5abb96
Nirmalya Ghosh <nirmalya.email@gmail.com>
2023-05-03 00:04:50 +0530

    feat: Updates UI

--- src/app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="w-full text-center p-4">
-      <h2>Not Found</h2>
-    </div>
-  );
-}
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,13 +4,18 @@ import { UserDetails } from "@/components/Navbar/UserDetails";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { Cpu } from "lucide-react";
+import { cn } from "@/lib/tailwind-utils";
 
 export const Navbar = async () => {
   const session = await getServerSession(authOptions);
 
   return (
     <div className="border-b text-sm">
-      <div className="px-4 h-16 container mx-auto flex items-center justify-between">
+      <div
+        className={cn("px-4 h-16 mx-auto flex items-center justify-between", {
+          container: !session,
+        })}
+      >
         <div className="space-x-4 flex items-center">
           <Link href="/" className="font-bold space-x-2 flex items-center">
             <Cpu /> <span>the-fullstack-app</span>
