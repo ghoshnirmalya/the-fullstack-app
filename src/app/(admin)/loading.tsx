0aa2488c22118cb3e951387504344e747f48df96
Nirmalya Ghosh <nirmalya.email@gmail.com>
2023-05-03 15:07:34 +0530

    chore: Uses cjs instead of js

--- src/app/(admin)/loading.tsx
+++ src/app/(admin)/loading.tsx
@@ -5,8 +5,9 @@ export default function Loading() {
     <div className="p-4 space-y-4">
       <Skeleton className="h-8 w-48" />
       <div className="w-full flex gap-4">
-        <Skeleton className="h-24 w-1/2" />
-        <Skeleton className="h-24 w-1/2" />
+        <Skeleton className="h-24 w-1/3" />
+        <Skeleton className="h-24 w-1/3" />
+        <Skeleton className="h-24 w-1/3" />
       </div>
     </div>
   );
