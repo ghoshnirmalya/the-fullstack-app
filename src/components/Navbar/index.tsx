56c28149370b476467ea5bc21dce6c1b1fcb2a75
Nirmalya Ghosh <nirmalya.email@gmail.com>
2023-05-03 17:59:09 +0530

    feat: Adds projects dropdown

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,28 @@
-import { authOptions } from "@/lib/auth";
 import { SignInButton } from "@/components/Navbar/SignInButton";
 import { UserDetails } from "@/components/Navbar/UserDetails";
-import { getServerSession } from "next-auth";
-import Link from "next/link";
-import { Cpu } from "lucide-react";
+import { ProjectsSelectorDropdown } from "@/components/ProjectsSelectorDropdown";
+import { authOptions } from "@/lib/auth";
+import { getApiUrl } from "@/lib/get-api-url";
 import { cn } from "@/lib/tailwind-utils";
+import { Project } from "@prisma/client";
+import { Cpu } from "lucide-react";
+import { getServerSession } from "next-auth";
+import { headers } from "next/headers";
+import Link from "next/link";
+import { notFound } from "next/navigation";
 
 export const Navbar = async () => {
   const session = await getServerSession(authOptions);
+
+  const response = await fetch(getApiUrl("api/projects"), {
+    headers: headers() as HeadersInit,
+  });
+
+  if (!response.ok) {
+    return notFound();
+  }
+
+  const projects: Project[] = await response.json();
 
   return (
     <div className="border-b text-sm">
@@ -16,10 +31,13 @@ export const Navbar = async () => {
           container: !session,
         })}
       >
-        <div className="space-x-4 flex items-center">
+        <div className="space-x-8 flex items-center">
           <Link href="/" className="font-bold space-x-2 flex items-center">
             <Cpu /> <span>the-fullstack-app</span>
           </Link>
+          <div className="block lg:hidden">
+            <ProjectsSelectorDropdown projects={projects} />
+          </div>
         </div>
         <div className="space-x-4">
           {session ? <UserDetails session={session} /> : <SignInButton />}
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,51 +1,27 @@
-"use client";
+import { ProjectsSelectorDropdown } from "@/components/ProjectsSelectorDropdown";
+import { SidebarLinks } from "@/components/Sidebar/SidebarLinks";
+import { getApiUrl } from "@/lib/get-api-url";
+import { Project } from "@prisma/client";
+import { headers } from "next/headers";
+import { notFound } from "next/navigation";
 
-import { cn } from "@/lib/tailwind-utils";
-import { BookOpen, FolderGit2, LayoutDashboard, Newspaper } from "lucide-react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+export const Sidebar = async () => {
+  const response = await fetch(getApiUrl("api/projects"), {
+    headers: headers() as HeadersInit,
+  });
 
-export const Sidebar = () => {
-  const pathname = usePathname();
+  if (!response.ok) {
+    return notFound();
+  }
+
+  const projects: Project[] = await response.json();
 
   return (
     <div className="flex flex-col space-y-2 border-r p-4 h-full w-full text-sm">
-      <Link
-        href="/dashboard"
-        className={cn("p-2 bg-transparent flex space-x-2 items-center", {
-          "bg-muted rounded": pathname === "/dashboard",
-        })}
-      >
-        <LayoutDashboard className="w-6 h-6" />{" "}
-        <span className="hidden lg:block">Dashboard</span>
-      </Link>
-      <Link
-        href="/projects"
-        className={cn("p-2 bg-transparent flex space-x-2 items-center", {
-          "bg-muted rounded": pathname.startsWith("/projects"),
-        })}
-      >
-        <FolderGit2 className="w-6 h-6" />{" "}
-        <span className="hidden lg:block">Projects</span>
-      </Link>
-      <Link
-        href="/forums"
-        className={cn("p-2 bg-transparent flex space-x-2 items-center", {
-          "bg-muted rounded": pathname.startsWith("/forums"),
-        })}
-      >
-        <Newspaper className="w-6 h-6" />{" "}
-        <span className="hidden lg:block">Forums</span>
-      </Link>
-      <Link
-        href="/blogs"
-        className={cn("p-2 bg-transparent flex space-x-2 items-center", {
-          "bg-muted rounded": pathname.startsWith("/blogs"),
-        })}
-      >
-        <BookOpen className="w-6 h-6" />{" "}
-        <span className="hidden lg:block">Blogs</span>
-      </Link>
+      <div className="hidden lg:block">
+        <ProjectsSelectorDropdown projects={projects} />
+      </div>
+      <SidebarLinks />
     </div>
   );
 };
